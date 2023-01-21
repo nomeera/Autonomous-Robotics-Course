b42f0754be6dc2f4401a95e16b6d172ffe65f100
Abdallah Nomeer <unomeera3@gmail.com>
2023-01-21 07:05:54 +0200

    initial commit again

---
+++ 1.Linux/Tasks/linux_exam/task1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ~/ROS/linux_exam
+mkdir -p this/is/my/linux/exam
+cd this/is/my/linux/exam
+touch my_file.py
+echo "This bash script has finished!"
+
---
+++ 1.Linux/Tasks/linux_exam/task3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+4 2 1
+cd ~/ROS/linux_exam/this/is/my/linux/exam
+rm *
+touch exam1.py exam2.py exam3.py
+chmod 754 exam1.py
+chmod 501 exam2.py
+chmod 241 exam3.py
+ls -la
