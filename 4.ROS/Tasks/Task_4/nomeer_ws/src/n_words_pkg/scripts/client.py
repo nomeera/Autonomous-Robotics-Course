38e368029828d2afc92b79e619480b39f7ed21e9
Abdallah Nomeer <abdallahnomeer173@gmail.com>
2023-02-11 02:23:20 +0200

    update n_words_pkg

--- 4.ROS/Tasks/Task_4/nomeer_ws/src/n_words_pkg/scripts/client.py
+++ 4.ROS/Tasks/Task_4/nomeer_ws/src/n_words_pkg/scripts/client.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
+import sys
 import rospy
-from n_words_pkg.srv import count_words , count_wordsRequest, count_wordsResponse
+from n_words_pkg.srv import count_words
 
 # Define a function that acts as the client
 def pub_words(words):
     # Wait until the service is available
-    rospy.wait_for_service("pub_words")  # Name of the service
+    rospy.wait_for_service("countWords")  # Name of the service
 
     try:
         # Create a proxy to the service
-        get_words = rospy.ServiceProxy("count_wordsx", count_words)
+        get_words = rospy.ServiceProxy("countWords", count_words)
 
         # Call the service and get the response
         resp = get_words(words)
@@ -20,6 +21,13 @@ def pub_words(words):
         print("Service Call failed %s", e)
 
 
+def usage():
+    return sys.argv[0]
+
+
 if __name__ == "__main__":
-    print("Count Number of words in string")
+    words = sys.argv[0]
+    print("The sentence you entered: %s", words)
+    count = pub_words(words)
+    print("Number of Words: %d")
     
--- 4.ROS/Tasks/Task_4/nomeer_ws/src/n_words_pkg/scripts/server.py
+++ 4.ROS/Tasks/Task_4/nomeer_ws/src/n_words_pkg/scripts/server.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 import rospy
-from n_of_words_pkg.srv import count_words, count_wordsRequest, count_wordsResponse
+from n_words_pkg.srv import count_words, count_wordsRequest, count_wordsResponse
 
 def num_of_words(req):
-    num_words = len(req.split(' '))
-    print("Number of Words is: %d", num_words)
-    return count_wordsResponse(num_words)
+    req.count = len(req.split(' '))
+    print("Number of Words is: %d", req.count)
+    return count_wordsResponse(req.count)
 
 def count_words_service():
     # Initialize the service node
     rospy.init_node("count_words_service")
     # Create the service with the name "count_words_service" and the function to handle the request
-    s = rospy.Service("count_words", count_words, num_of_words)
+    s = rospy.Service("countWords", count_words, num_of_words)
     #  Print a message indicating that the server is ready
     print("Ready to count number of words")
     # Spin to keep the node running
